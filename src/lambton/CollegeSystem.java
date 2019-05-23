c87828056c07d9148e1aaa8504a02863694186bc
Shivani <shivanidhiman1619@gmail.com>
2019-05-23 18:56:55 -0400

    [Shivani] Hello World added

--- src/lambton/CollegeSystem.java
+++ src/lambton/CollegeSystem.java
@@ -2,8 +2,8 @@ package lambton;
 
 public class CollegeSystem
 {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
     }
-
-
 }
